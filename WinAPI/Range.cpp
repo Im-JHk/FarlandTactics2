03830354575cd56f06cc27f1d39c1182b6940584
Im-JHk <99063568+Im-JHk@users.noreply.github.com>
2022-08-10 17:33:18 +0900

    Range issue modify

--- WinAPI/Range.cpp
+++ WinAPI/Range.cpp
@@ -40,26 +40,26 @@ void Range::FindFourDirectionTile(Tile* tile, int cost, int maxCost)
 	if (row - 1 >= 0) {
 		if (mvTile[left]->GetType() == eObjectType::TILE) {
 			msRange.insert(left);
-			FindFourDirectionTile(mvTile[left], cost + 1, maxCost);
 		}
+		FindFourDirectionTile(mvTile[left], cost + 1, maxCost);
 	}
 	if (row + 1 < miRowMax) {
 		if (mvTile[right]->GetType() == eObjectType::TILE) {
 			msRange.insert(right);
-			FindFourDirectionTile(mvTile[right], cost + 1, maxCost);
 		}
+		FindFourDirectionTile(mvTile[right], cost + 1, maxCost);
 	}
 	if (col - 1 >= 0) {
 		if (mvTile[up]->GetType() == eObjectType::TILE) {
 			msRange.insert(up);
-			FindFourDirectionTile(mvTile[up], cost + 1, maxCost);
 		}
+		FindFourDirectionTile(mvTile[up], cost + 1, maxCost);
 	}
 	if (col + 1 < miColMax) {
 		if (mvTile[down]->GetType() == eObjectType::TILE) {
 			msRange.insert(down);
-			FindFourDirectionTile(mvTile[down], cost + 1, maxCost);
 		}
+		FindFourDirectionTile(mvTile[down], cost + 1, maxCost);
 	}
 }
 
@@ -79,25 +79,25 @@ void Range::FindFourDirectionTileExceptRceceiver(Tile * tile, eObjectType rcvTyp
 	if (row - 1 >= 0) {
 		if (mvTile[left]->GetType() == eObjectType::TILE || mvTile[left]->GetType() == rcvType) {
 			msRange.insert(left);
-			FindFourDirectionTileExceptRceceiver(mvTile[left], rcvType, cost + 1, maxCost);
 		}
+		FindFourDirectionTileExceptRceceiver(mvTile[left], rcvType, cost + 1, maxCost);
 	}
 	if (row + 1 < miRowMax) {
 		if (mvTile[right]->GetType() == eObjectType::TILE || mvTile[right]->GetType() == rcvType) {
 			msRange.insert(right);
-			FindFourDirectionTileExceptRceceiver(mvTile[right], rcvType, cost + 1, maxCost);
 		}
+		FindFourDirectionTileExceptRceceiver(mvTile[right], rcvType, cost + 1, maxCost);
 	}
 	if (col - 1 >= 0) {
 		if (mvTile[up]->GetType() == eObjectType::TILE || mvTile[up]->GetType() == rcvType) {
 			msRange.insert(up);
-			FindFourDirectionTileExceptRceceiver(mvTile[up], rcvType, cost + 1, maxCost);
 		}
+		FindFourDirectionTileExceptRceceiver(mvTile[up], rcvType, cost + 1, maxCost);
 	}
 	if (col + 1 < miColMax) {
 		if (mvTile[down]->GetType() == eObjectType::TILE || mvTile[down]->GetType() == rcvType) {
 			msRange.insert(down);
-			FindFourDirectionTileExceptRceceiver(mvTile[down], rcvType, cost + 1, maxCost);
 		}
+		FindFourDirectionTileExceptRceceiver(mvTile[down], rcvType, cost + 1, maxCost);
 	}
 }
